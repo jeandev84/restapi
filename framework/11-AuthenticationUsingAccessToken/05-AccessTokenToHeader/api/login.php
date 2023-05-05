427c2b04bbd4082ad0c2bd1b878c3a757f857b66
Жан-Клод <jeanyao@ymail.com>
2023-05-05 03:21:17 +0300

    Access Token to Header && Bearer Authorization

---
+++ framework/11-AuthenticationUsingAccessToken/05-AccessTokenToHeader/api/login.php
@@ -0,0 +1,107 @@
+<?php
+declare(strict_types=1);
+
+# CLIENT
+# http://localhost:8000/api/login.php
+require __DIR__.'/bootstrap.php';
+
+$jsonCredentials = '{
+    "username": "demo",
+    "password": "demo"
+}';
+
+
+if ($_SERVER["REQUEST_METHOD"] !== "POST") {
+      // Method not allowed
+      http_response_code(405);
+      header("Allow: POST");
+      exit;
+}
+
+$data = (array) json_decode(file_get_contents("php://input"), true);
+
+if (! array_key_exists('username', $data) ||
+    ! array_key_exists('password', $data)) {
+
+     // Bad request
+     http_response_code(400);
+     echo json_encode(["message" => "missing login credentials"]);
+     exit;
+}
+
+
+# Check user by username
+$database = new Database($_ENV["DB_HOST"], $_ENV["DB_NAME"], $_ENV["DB_USER"], $_ENV["DB_PASS"]);
+
+$userGateway = new UserGateway($database);
+
+$user = $userGateway->getByUsername($data["username"]);
+
+if ($user === false) {
+    # Unauthorized
+    http_response_code(401);
+    echo json_encode(["message" => "invalid authentication"]);
+    exit;
+}
+
+
+# Verify User password
+if (! password_verify($data["password"], $user["password_hash"])) {
+     # Unauthorized
+     http_response_code(401);
+     echo json_encode(["message" => "invalid authentication"]);
+     exit;
+}
+
+/*
+# echo json_encode($user);
+# echo json_encode("Successful authentication");
+# Encoding User credentials
+# https://www.php.net/manual/en/function.base64-encode.php
+
+$payload = [
+  "id" => $user["id"],
+  "name" => $user["name"],
+  "roles" => $user["roles"]
+];
+
+$ php -a
+Interactive shell
+
+php > $encoded = base64_encode("hello world");
+php > echo $encoded;
+aGVsbG8gd29ybGQ=
+php > echo base64_decode($encoded);
+hello world
+php >
+
+*/
+
+
+$payload = [
+  "id" => $user["id"],
+  "name" => $user["name"]
+];
+
+
+
+$accessToken = base64_encode(json_encode($payload));
+
+echo json_encode([
+    "access_token" => $accessToken
+]);
+
+/*
+{
+    "access_token": "eyJpZCI6MywibmFtZSI6IkRlbW8ifQ=="
+}
+
+php -a
+Interactive shell
+
+php > echo base64_decode("eyJpZCI6MywibmFtZSI6IkRlbW8ifQ==");
+{"id":3,"name":"Demo"}
+php >
+*/
+
+
